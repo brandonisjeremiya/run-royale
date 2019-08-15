aeadce43e09c259ce0f7f1c447852bb3835efde9
Brandon <jbrandonis7000@gmail.com>
2019-08-15 17:21:10 -0400

    added destroy me script
    added script to destroy instantiated object after a variable amount of time

---
+++ Assets/scripts/destroyMe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyMe : MonoBehaviour
+{
+
+	public float destroySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject,destroySpeed);
+    }
+}
